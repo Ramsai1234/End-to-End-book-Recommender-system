97a207d4b158ea888303f83483d3b3a70b6bbfab
P.Ramsai <ponugotiramsai@gmail.com>
2026-04-08 15:45:28 +0530

    logging exception utils

---
+++ app.py
@@ -0,0 +1,10 @@
+from books_recommender.logger.log import logging
+from books_recommender.exception.exception_handler import AppException
+import sys
+
+
+#logging.info("starting the application...")
+try:
+    a=1/0
+except Exception as e:
+    raise AppException(e,sys)
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="books_recommender",
+    version="0.0.1",
+    packages=find_packages(),
+    author_email="Ramsai@gmail.com",
+    author="Ramsai",
+    install_requires=[]
+)
